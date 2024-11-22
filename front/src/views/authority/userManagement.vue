8c8e9d463c5b098e01fc7f196e5291701a49e809
huangchuanjian <huangssssx@qq.com>
2024-11-22 18:04:00 +0800

    美化搜索栏

--- front/src/views/authority/userManagement.vue
+++ front/src/views/authority/userManagement.vue
@@ -1,14 +1,32 @@
 <template>
-    <el-form ref="searchFormRef" :model="searchForm" inline @submit.prevent>
-        <el-form-item label="用户名" prop="username">
-            <el-input v-model="searchForm.username" placeholder="请输入用户名" @keyup.enter="search"/>
-        </el-form-item>
+    <div class="search-container">
+        <el-form ref="searchFormRef" :model="searchForm" inline @submit.prevent class="search-form">
+            <el-form-item label="用户名" prop="username">
+                <el-input 
+                    v-model="searchForm.username" 
+                    placeholder="请输入用户名" 
+                    @keyup.enter="search"
+                    :prefix-icon="Search"
+                    class="search-input"
+                />
+            </el-form-item>
 
-        <el-form-item>
-            <el-button type="primary" @click="resetForm">重置</el-button>
-            <el-button type="primary" @click="search">搜索</el-button>
-        </el-form-item>
-    </el-form>
+            <el-form-item class="search-buttons">
+                <el-button 
+                    type="default" 
+                    @click="resetForm"
+                    :icon="Refresh"
+                    class="reset-button"
+                >重置</el-button>
+                <el-button 
+                    type="primary" 
+                    @click="search"
+                    :icon="Search"
+                    class="search-button"
+                >搜索</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
     <el-table :data="tableData" style="width: 100%">
       <el-table-column prop="date" label="Date" width="180" />
       <el-table-column prop="name" label="Name" width="180" />
@@ -22,6 +40,7 @@ import { getUserInfo } from '@/api/user'
 import type { UserInfo } from '@/api/user'
 import { FormInstance } from 'element-plus'
 import { debouncedSearch } from '@/composeable/search'
+import { Search, Refresh } from '@element-plus/icons-vue'
 
 // 定义表格数据
 const tableData = ref([])
@@ -73,3 +92,52 @@ onMounted(() => {
   fetchUserInfo()
 })
 </script>
+
+<style scoped>
+.search-container {
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.05);
+    margin-bottom: 20px;
+}
+
+.search-form {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 10px;
+}
+
+.search-input {
+    width: 240px;
+}
+
+.search-input :deep(.el-input__wrapper) {
+    border-radius: 4px;
+}
+
+.search-buttons {
+    margin-left: auto;
+    margin-right: 0;
+}
+
+.search-buttons :deep(.el-form-item__content) {
+    display: flex;
+    gap: 10px;
+}
+
+.reset-button {
+    background-color: #f4f4f5;
+    border-color: #dcdfe6;
+}
+
+.reset-button:hover {
+    background-color: #e9e9eb;
+    border-color: #dcdfe6;
+}
+
+.search-button {
+    margin-left: 0;
+}
+</style>
